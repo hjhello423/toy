6062385d5a561e656f8e29d992eaabd377312e2b
hjdeepsleep <hjdeepsleep@gmail.com>
2020-08-31 21:34:34 +0900

    init

---
+++ src/test/java/com/github/hjdeepsleep/toy/ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hjdeepsleep.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
