115ce59e36ea36d9499c0866fc70f71678b4cf75
hjdeepsleep <hjdeepsleep@gmail.com>
2020-09-03 23:48:15 +0900

    test : 엔티티 동작 테스트

--- src/test/java/com/github/hjdeepsleep/toy/ToyApplicationTests.java
+++ src/test/java/com/github/hjdeepsleep/toy/ToyApplicationTests.java
@@ -6,7 +6,6 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -15,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
-@Commit
 class ToyApplicationTests {
 
     @Autowired
