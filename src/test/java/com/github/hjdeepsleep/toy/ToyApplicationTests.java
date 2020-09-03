6c55a35eaec46469a00ae3eb8a11356734d13491
hjdeepsleep <hjdeepsleep@gmail.com>
2020-09-03 22:28:19 +0900

    feat : h2 db 연결 및 테스

--- src/test/java/com/github/hjdeepsleep/toy/ToyApplicationTests.java
+++ src/test/java/com/github/hjdeepsleep/toy/ToyApplicationTests.java
@@ -6,6 +6,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
+@Commit
 class ToyApplicationTests {
 
     @Autowired
