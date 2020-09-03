a364a13d55d0cddc635d7dde8788048959bf065c
hjdeepsleep <hjdeepsleep@gmail.com>
2020-09-03 23:47:54 +0900

    feat : 엔티티 추가

---
+++ src/main/java/com/github/hjdeepsleep/toy/domain/Team.java
@@ -0,0 +1,30 @@
+package com.github.hjdeepsleep.toy.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
