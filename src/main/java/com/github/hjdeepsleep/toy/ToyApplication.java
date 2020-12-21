7d8e85fa8f56d45f4942e99c32ba5185d80db04b
hjdeepsleep <hjdeepsleep@gmail.com>
2020-12-22 00:22:25 +0900

    feat : Hibernate5Module
    - Hibernate5Module 이용하여 연관관계 자동 강제 초기화

--- src/main/java/com/github/hjdeepsleep/toy/ToyApplication.java
+++ src/main/java/com/github/hjdeepsleep/toy/ToyApplication.java
@@ -1,5 +1,6 @@
 package com.github.hjdeepsleep.toy;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +17,15 @@ public class ToyApplication {
 
     @Bean
     public JPAQueryFactory queryFactory(EntityManager em) {
-		return new JPAQueryFactory(em);
+        return new JPAQueryFactory(em);
     }
+
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+//        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true); // lazy로딩 강제로 초기화시킴
+
+        return hibernate5Module;
+    }
+
 }
