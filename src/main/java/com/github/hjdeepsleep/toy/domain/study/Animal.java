44caf3a5830cf87d1e7987a96384c0aaf1cb4e89
hjdeepsleep <hjdeepsleep@gmail.com>
2020-12-27 04:01:48 +0900

    test : ObjectMapper 탐구
    - serialize, deserialize 각 조건 테스트 해보기

---
+++ src/main/java/com/github/hjdeepsleep/toy/domain/study/Animal.java
@@ -0,0 +1,32 @@
+package com.github.hjdeepsleep.toy.domain.study;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@ToString
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+//@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+public class Animal {
+
+    public Animal(String name, int weight, Integer age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    private String name;
+    private int weight;
+    private Integer age;
+
+    public String getColor() {
+        return "red";
+    }
+
+    public String setColor() {
+        return "blue";
+    }
+
+}
