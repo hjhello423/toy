184467dc4d2e861c572f3eb26473befe96a22e8e
00hongjun <00hongjun@gmail.com>
2021-10-10 13:09:38 +0900

    <feat> 연습문제 풀이 추가

---
+++ study-design-pattern/src/main/java/com/my/studydesignpattern/chapter1/Account.java
@@ -0,0 +1,24 @@
+package com.my.studydesignpattern.chapter1;
+
+/*
+chapter1 연습문제 7번, 55p
+ */
+public class Account {
+
+    private int id;
+    private String owner;
+    private double balance;
+
+    public void deposit() {
+
+    }
+
+    public void withdraw() {
+
+    }
+
+    protected double getBalance() {
+        return 0;
+    }
+
+}
