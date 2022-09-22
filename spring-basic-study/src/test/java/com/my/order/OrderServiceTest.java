672ff29c474583631ae93f4682bb7948f4dc325d
00hongjun <00hongjun@gmail.com>
2022-09-22 22:07:57 +0900

    test: 주문 테스트 코드

---
+++ spring-basic-study/src/test/java/com/my/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package com.my.order;
+
+import com.my.member.Grade;
+import com.my.member.Member;
+import com.my.member.MemberService;
+import com.my.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
