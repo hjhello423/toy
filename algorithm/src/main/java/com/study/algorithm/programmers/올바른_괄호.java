13ecb7ca82ef128febf26c5e9ace3eff2a31eba6
hjhello423 <hjhello423@gmail.com>
2023-06-22 18:25:26 +0900

    알고리즘 풀이 - 올바른_괄호.java

---
+++ algorithm/src/main/java/com/study/algorithm/programmers/올바른_괄호.java
@@ -0,0 +1,30 @@
+package com.study.algorithm.programmers;
+
+import java.util.Stack;
+
+public class 올바른_괄호 {
+
+    public static void main(String[] args) {
+        boolean solution = solution("(()(");
+        System.out.println(solution);
+    }
+
+    public static boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char str = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(str);
+            } else if (str == '(') {
+                stack.push(str);
+            } else {
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+}
