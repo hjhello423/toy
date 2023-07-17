2d1cdec0910b7603b00c0837d2d635021ba9629f
hjhello423 <hjhello423@gmail.com>
2023-07-17 11:52:24 +0900

    알고리즘 풀이 - 구간 합 구하기 4

---
+++ algorithm/src/main/java/com/study/algorithm/baekjoon/No11659.java
@@ -0,0 +1,58 @@
+package com.study.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 구간 합 구하기 4
+ * https://www.acmicpc.net/problem/11659
+ *
+ * 문제
+ * 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다.
+ * 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+ *
+ * 출력
+ * 총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+ *
+ * 제한
+ * 1 ≤ N ≤ 100,000
+ * 1 ≤ M ≤ 100,000
+ * 1 ≤ i ≤ j ≤ N
+ */
+public class No11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] sum = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (i == 0) {
+                sum[i] = num;
+            } else {
+                sum[i] = sum[i - 1] + num;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+
+            if (start == 0) {
+                System.out.println(sum[end]);
+            } else {
+                System.out.println(sum[end] - sum[start - 1]);
+            }
+        }
+    }
+}
